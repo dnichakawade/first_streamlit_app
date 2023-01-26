ef1c285a6060c97012d107ffbd56910235ca55f4
dnichakawade <123527822+dnichakawade@users.noreply.github.com>
2023-01-26 12:20:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title("This is my first Streamlit python program.")
+streamlit.title('This is my first Streamlit python program.')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard Boiled Free-Range Egg')
