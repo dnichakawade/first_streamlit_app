2e0594f362089e8c3e3d6fbc517995d7fd242db7
dnichakawade <123527822+dnichakawade@users.noreply.github.com>
2023-02-02 21:46:46 -0500

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,3 +1,6 @@
+
+streamlit.header('Fruityvice fruit advice')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
