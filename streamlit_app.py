1400c4ca566f88549dc31dc9ac9eecd2eb41c9e7
dnichakawade <123527822+dnichakawade@users.noreply.github.com>
2023-02-17 08:03:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,11 @@ import streamlit
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets[snowflake])
+try:
+  my_cnx = snowflake.connector.connect(**streamlit.secrets[snowflake])
+except Exception as e:
+  return [[e, e]]
+
 my_cur = my_cnx.cursor()
 my_cur.execute("select CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
