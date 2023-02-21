25664742f427dc71bdf79c5328b0779497415c19
dnichakawade <123527822+dnichakawade@users.noreply.github.com>
2023-02-21 13:55:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from  fruityvice;")
+rows = run_query("use WAREHOUSE COMPUTE_WH; SELECT * from  fruityvice;")
 
 # Print results.
 for row in rows:
